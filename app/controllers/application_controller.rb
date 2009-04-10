f3ee400d3471424bd8f35f29e915c55d272159fb
David Dunwoody <david.dunwoody@gmail.com>
2009-04-10 14:30:01 +0100

    Update ApplicationController with changes from Rails 2.2.2 to 2.3.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,11 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'de3cf3203e6ce529457f1cbdd5a8bf6c'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
   before_filter :set_nav_context
 
   private
