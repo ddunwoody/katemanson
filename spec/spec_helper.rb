a86660ee845fdf47664bb48e3bd8da8c8b0a7b10
David Dunwoody <david.dunwoody@gmail.com>
2009-04-10 13:59:23 +0100

    Update RSpec/RSpec-Rails version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,16 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require 'spec'
+ENV["RAILS_ENV"] ||= 'test'
+require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
+require 'spec/autorun'
 require 'spec/rails'
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
   # lines, delete config/database.yml and disable :active_record
   # in your config/boot.rb
+
+  # Commented out by David
   #config.use_transactional_fixtures = true
   #config.use_instantiated_fixtures  = false
   #config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
@@ -43,5 +45,5 @@ Spec::Runner.configure do |config|
   #
   # == Notes
   # 
-  # For more information take a look at Spec::Example::Configuration and Spec::Runner
+  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
 end
