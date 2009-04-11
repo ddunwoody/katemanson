0980eee48a4a74b1485003b8a5bcaff661365401
David Dunwoody <david.dunwoody@gmail.com>
2009-04-11 16:27:41 +0100

    Add basic authorization mechanism and admin? helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,25 @@
+require 'digest/sha1'
+
 class ApplicationController < ActionController::Base
   include SslRequirement
   before_filter :set_nav_context
-
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
-  # Scrub sensitive parameters from your log
+  protect_from_forgery
   filter_parameter_logging :password
 
-  private
+  helper_method :admin?
+
+  protected
+
+  def authorize
+    unless admin?
+      logger.warn "Unauthorized #{request.method.to_s.upcase} on #{request.path} from #{request.remote_ip}"
+      redirect_to home_path
+    end
+  end
+
+  def admin?
+    Digest::SHA2.hexdigest(session[:password]) == YAML.load_file("#{RAILS_ROOT}/config/admin.yml")['password'] unless session[:password].nil?
+  end
 
   def set_nav_context
     # TODO: figure out how to sort action_methods appropriately
